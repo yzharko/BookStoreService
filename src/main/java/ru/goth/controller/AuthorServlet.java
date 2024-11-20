0fa4abb0d42f9db30b9a1d76e2e1a34bef7c860b
Zharko <zharko.aktobe@gmail.com>
2024-11-20 11:31:20 +0300

    code: Author servlet with doGet  has been implemented

---
+++ src/main/java/ru/goth/controller/AuthorServlet.java
@@ -0,0 +1,33 @@
+package ru.goth.controller;
+
+import ru.goth.entity.dto.AuthorDTO;
+import ru.goth.service.AuthorService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "getAuthor", value = "/getAuthor")
+public class AuthorServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/html");
+        long id = Long.parseLong(request.getParameter("author_id"));
+
+        AuthorService authorService = new AuthorService();
+        AuthorDTO authorDTO = authorService.getById(id);
+
+        request.setAttribute("author", authorDTO);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/author.jsp");
+
+        try {
+            requestDispatcher.forward(request, response);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
