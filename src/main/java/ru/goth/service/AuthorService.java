a418bb9486d2ff96c2b19a23183a506086296112
Zharko <zharko.aktobe@gmail.com>
2024-11-20 14:56:08 +0300

    code: set, update, delete

--- src/main/java/ru/goth/service/AuthorService.java
+++ src/main/java/ru/goth/service/AuthorService.java
@@ -4,6 +4,8 @@ import ru.goth.entity.Author;
 import ru.goth.entity.dto.AuthorDTO;
 import ru.goth.repository.AuthorDAO;
 
+import java.util.logging.Logger;
+
 public class AuthorService {
     public AuthorDTO getById(long id) {
         AuthorDAO authorDAO = new AuthorDAO();
@@ -13,5 +15,22 @@ public class AuthorService {
         authorDTO.setName(author.getName());
 
         return authorDTO;
+    }
+    public void setByDTO(AuthorDTO authorDTO) {
+        AuthorDAO authorDAO = new AuthorDAO();
+        authorDAO.setAuthor(authorDTO.getName());
+    }
+    public void update(long id, AuthorDTO authorDTO) {
+        AuthorDAO authorDAO = new AuthorDAO();
+        if (authorDAO.getAuthor(id) == null) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.info("No such author");
+        } else {
+            authorDAO.updateAuthor(id, authorDTO.getName());
+        }
+    }
+    public void deleteByDTO(AuthorDTO authorDTO) {
+        AuthorDAO authorDAO = new AuthorDAO();
+        authorDAO.deleteAuthor(authorDTO.getName());
     }
 }
