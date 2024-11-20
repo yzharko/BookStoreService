16385b790794502d1e91023c832826d7e45ac2af
Zharko <zharko.aktobe@gmail.com>
2024-11-20 10:51:56 +0300

    code: DataBaseConfig has been implemented

---
+++ src/main/java/ru/goth/config/DataBaseConfig.java
@@ -0,0 +1,24 @@
+package ru.goth.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+
+public class DataBaseConfig {
+    private static HikariDataSource dataSource;
+    static {
+        ConfigLoader configLoader = new ConfigLoader();
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(configLoader.getDriverName());
+        config.setJdbcUrl(configLoader.getDbUrl());
+        config.setUsername(configLoader.getDbUsername());
+        config.setPassword(configLoader.getDbPassword());
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static DataSource getDataSource() {
+        return dataSource;
+    }
+}
