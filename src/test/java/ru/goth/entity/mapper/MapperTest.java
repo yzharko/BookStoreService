af13f2c658b2729a5320e543e55f2a3e81fede92
Zharko <zharko.aktobe@gmail.com>
2024-11-20 11:37:30 +0300

    test: semple Author mapper test

---
+++ src/test/java/ru/goth/entity/mapper/MapperTest.java
@@ -0,0 +1,25 @@
+package ru.goth.entity.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import ru.goth.entity.Author;
+import ru.goth.entity.dto.AuthorDTO;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapperTest {
+    @Test
+    void sameAuthorNameInEntityAndDTO() {
+        final long id = 1L;
+        final String name = "Pablo";
+        Author author = new Author();
+        author.setId(id);
+        author.setName(name);
+        AuthorMapper authorMapper = Mappers.getMapper(AuthorMapper.class);
+        AuthorDTO authorDTO = authorMapper.toAuthorDTO(author);
+
+        assertEquals(author.getName(), authorDTO.getName());
+    }
+
+}
+
