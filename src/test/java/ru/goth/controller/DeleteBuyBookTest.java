99cdb342805f3b4b21b7066e5bcac5e2fe387841
Zharko <zharko.aktobe@gmail.com>
2024-11-23 13:47:17 +0300

    test: BuyBook controller tests

---
+++ src/test/java/ru/goth/controller/DeleteBuyBookTest.java
@@ -0,0 +1,49 @@
+package ru.goth.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import ru.goth.controller.buybookservlets.DeleteBuyBook;
+import ru.goth.service.BuyBookService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+public class DeleteBuyBookTest {
+
+    private BuyBookService mockBuyBookService;
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private DeleteBuyBook deleteBuyBook;
+    private RequestDispatcher requestDispatcher;
+
+    @Before
+    public void setup() {
+        mockBuyBookService = mock(BuyBookService.class);
+        request = mock(HttpServletRequest.class);
+        response = mock(HttpServletResponse.class);
+        requestDispatcher = mock(RequestDispatcher.class);
+        deleteBuyBook = new DeleteBuyBook(mockBuyBookService);
+    }
+
+    @Test
+    public void testDoDelete() throws ServletException, IOException {
+        final long buyBookId = 1L;
+        when(request.getParameter("id")).thenReturn(String.valueOf(buyBookId));
+
+        final String jsp = "/index.jsp";
+        when(request.getRequestDispatcher(jsp)).thenReturn(requestDispatcher);
+
+        deleteBuyBook.doDelete(request, response);
+
+        verify(mockBuyBookService).deleteById(any(long.class));
+        verify(requestDispatcher).forward(request, response);
+    }
+}
--- src/test/java/ru/goth/controller/PutBuyBookTest.java
+++ src/test/java/ru/goth/controller/PutBuyBookTest.java
@@ -2,7 +2,7 @@ package ru.goth.controller;
 
 import org.junit.Before;
 import org.junit.Test;
-import ru.goth.controller.buybookservlets.SetBuyBook;
+import ru.goth.controller.buybookservlets.PutBuyBook;
 import ru.goth.entity.dto.BookDTO;
 import ru.goth.entity.dto.BuyBookDTO;
 import ru.goth.entity.dto.BuyDTO;
@@ -14,13 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-public class SetBuyBookTest {
+public class PutBuyBookTest {
     private BuyBookService mockBuyBookService;
     private HttpServletRequest request;
     private HttpServletResponse response;
-    private SetBuyBook setBuyBook;
+    private PutBuyBook putBuyBook;
     private RequestDispatcher requestDispatcher;
 
     @Before
@@ -29,22 +30,31 @@ public class SetBuyBookTest {
         request = mock(HttpServletRequest.class);
         response = mock(HttpServletResponse.class);
         requestDispatcher = mock(RequestDispatcher.class);
-        setBuyBook = new SetBuyBook(mockBuyBookService);
+        putBuyBook = new PutBuyBook(mockBuyBookService);
     }
 
     @Test
-    public void testDoPost() throws ServletException, IOException {
+    public void testDoPut() throws ServletException, IOException {
+
+        final long buyBookId = 1L;
+        final long buyId = 1L;
         final String description = "Deliver only during night time";
         final String client = "GothGamerGhoul282";
+
+        final long bookId = 1L;
         final String title = "Occult encyclopedia";
         final long authorId = 1L;
         final String genre = "Occult";
         final float price = 228.00F;
         final int bookAmount = 1;
+
         final int buyBookAmount = 1;
 
+        when(request.getParameter("buyBook.id")).thenReturn(String.valueOf(buyBookId));
+        when(request.getParameter("buy.id")).thenReturn(String.valueOf(buyId));
         when(request.getParameter("description")).thenReturn(description);
         when(request.getParameter("client")).thenReturn(client);
+        when(request.getParameter("book.id")).thenReturn(String.valueOf(bookId));
         when(request.getParameter("title")).thenReturn(title);
         when(request.getParameter("author.id")).thenReturn(String.valueOf(authorId));
         when(request.getParameter("genre")).thenReturn(genre);
@@ -56,9 +66,10 @@ public class SetBuyBookTest {
         when(request.getRequestDispatcher(jsp)).thenReturn(requestDispatcher);
         when(response.getStatus()).thenReturn(HttpServletResponse.SC_CREATED);
 
-        setBuyBook.doPost(request, response);
+        putBuyBook.doPut(request, response);
 
-        verify(mockBuyBookService).setByDTO(any(BuyDTO.class), any(BookDTO.class), any(BuyBookDTO.class));
+        verify(mockBuyBookService).update(any(long.class), any(BuyDTO.class), any(long.class),
+                any(BookDTO.class), any(long.class), any(BuyBookDTO.class));
         verify(requestDispatcher).forward(request, response);
     }
 }
