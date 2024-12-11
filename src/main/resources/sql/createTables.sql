0760e4ebff4dc473e55f9d45e2424628f62a97ef
Zharko <zharko.aktobe@gmail.com>
2024-12-11 16:06:48 +0300

    sql: scripts for table creation and data insertion have been added

---
+++ src/main/resources/sql/createTables.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE author (
+    author_id SERIAL PRIMARY KEY,
+    name_author VARCHAR(50)
+);
+
+CREATE TABLE book (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(50),
+    author_id INT NOT NULL,
+    genre VARCHAR(50),
+    price DECIMAL(8, 2),
+    amount INT,
+    FOREIGN KEY (author_id)
+        REFERENCES author (author_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE buy(
+    buy_id SERIAL PRIMARY KEY,
+    buy_description VARCHAR(100),
+    client VARCHAR(50)
+);
+
+CREATE TABLE buy_book (
+    buy_book_id SERIAL PRIMARY KEY,
+    buy_id INT,
+    book_id INT,
+    amount INT,
+    FOREIGN KEY (buy_id) REFERENCES buy (buy_id),
+    FOREIGN KEY (book_id) REFERENCES book (book_id)
+);
---
+++ src/main/resources/sql/insertTables.sql
@@ -0,0 +1,33 @@
+﻿INSERT INTO author (name_author)
+VALUES ('Bulgakov M.A.'),
+       ('Dostoevsky F.M.'),
+       ('Esenin S.A.'),
+       ('Pasternak B.L.'),
+       ('Lermontov M.Y.');
+
+INSERT INTO book (title, author_id, genre, price, amount)
+VALUES  ('Master and Margarita', 1, 'Novel', 670.99, 3),
+        ('The White Guard ', 1, 'Novel', 540.50, 5),
+        ('Idiot', 2, 'Novel', 460.00, 10),
+        ('Karamazov Brothers', 2, 'Novel', 799.01, 2),
+        ('Player', 2, 'Novel', 480.50, 10),
+        ('Poems and Verses', 3, 'Poetry', 650.00, 15),
+        ('Black man', 3, 'Poetry', 570.20, 6),
+        ('Lirics', 4, 'Poetry', 518.99, 2);
+
+INSERT INTO buy (buy_description, client)
+VALUES ('Delivery only in the evening', 'Baranov P.'),
+       (NULL, 'Semyonov I.'),
+       ('Pack each book individually', 'Abramova K.'),
+       (NULL, 'Baranov P.');
+
+INSERT INTO buy_book(buy_id, book_id, amount)
+VALUES (1, 1, 1),
+       (1, 7, 2),
+       (1, 3, 1),
+       (2, 8, 2),
+       (3, 3, 2),
+       (3, 2, 1),
+       (3, 1, 1),
+       (4, 5, 1);
+       
