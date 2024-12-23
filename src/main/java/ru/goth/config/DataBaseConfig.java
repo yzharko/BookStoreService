5e4f34d8208b4e42a8f7e2a8db2dbb1e59befb88
Zharko <zharko.aktobe@gmail.com>
2024-12-24 01:44:42 +0300

    config: db config fix and web config added

--- src/main/java/ru/goth/config/DataBaseConfig.java
+++ src/main/java/ru/goth/config/DataBaseConfig.java
@@ -3,12 +3,14 @@ package ru.goth.config;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
 
 @Configuration
+@ComponentScan(basePackages = "ru.goth")
 public class DataBaseConfig {
 
     @Bean
---
+++ src/main/java/ru/goth/config/WebConfig.java
@@ -0,0 +1,11 @@
+package ru.goth.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ru.goth")
+public class WebConfig implements WebMvcConfigurer {}
